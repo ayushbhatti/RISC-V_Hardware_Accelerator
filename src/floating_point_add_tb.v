4e6477acb86dae431b2f3cf4264324b046290722
Owen <osowatzke@gmail.com>
2024-10-15 19:17:28 -0700

    Adding clock and reset generator modules

--- src/floating_point_add_tb.v
+++ src/floating_point_add_tb.v
@@ -5,16 +5,8 @@ module floating_point_add_tb;
     parameter CLK_PERIOD = 10;
     parameter RESET_TIME = 100;
     
-    reg clk;
-    reg rst;
-    
-    initial begin
-        clk <= 1;
-        rst <= 1;
-        #RESET_TIME rst <= ~rst;
-    end
-    
-    always #(CLK_PERIOD/2) clk <= ~clk;
+    wire clk;
+    wire rst;
 
     wire [31:0] dataA;
     wire [31:0] dataB;
@@ -22,6 +14,9 @@ module floating_point_add_tb;
     
     wire sumValid;
     wire [31:0] sum;
+    
+    clk_gen #(.CLK_PERIOD(CLK_PERIOD)) clk_gen_i (.clkOut(clk));
+    rst_gen #(.RESET_TIME(RESET_TIME)) rst_gen_i (.rstOut(rst));
     
     file_driver driver (
         .clkIn(clk),
