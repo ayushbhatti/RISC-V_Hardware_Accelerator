12b2204d49f95115369a2fbcb406aeb2f31cb0b8
ayushbhatti <ayushvb99@gmail.com>
2024-10-29 20:49:24 -0700

    updated_code_convo_for_dynamic_alloction_V2_AB

--- models/benchmark_convo.c
+++ models/benchmark_convo.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define IMAGE_SIZE 512  // Define the size of the input image (IMAGE_SIZE x IMAGE_SIZE)
-#define KERNEL_SIZE 3   // Define the size of the kernel (KERNEL_SIZE x KERNEL_SIZE)
+#define IMAGE_SIZE 1024  // Define the size of the input image (IMAGE_SIZE x IMAGE_SIZE)
+#define KERNEL_SIZE 5    // Define the size of the kernel (KERNEL_SIZE x KERNEL_SIZE)
 
 // Function to perform 2D convolution
-void cpu_convolution(float image[IMAGE_SIZE][IMAGE_SIZE], float kernel[KERNEL_SIZE][KERNEL_SIZE], float output[IMAGE_SIZE][IMAGE_SIZE]) {
+void cpu_convolution(float **image, float **kernel, float **output) {
     int i, j, m, n;
     int kernel_radius = KERNEL_SIZE / 2;
 
@@ -32,10 +32,18 @@ void cpu_convolution(float image[IMAGE_SIZE][IMAGE_SIZE], float kernel[KERNEL_SI
 }
 
 int main() {
-    // Initialize image and kernel arrays
-    float image[IMAGE_SIZE][IMAGE_SIZE];
-    float kernel[KERNEL_SIZE][KERNEL_SIZE];
-    float output[IMAGE_SIZE][IMAGE_SIZE];
+    // Dynamically allocate memory for image, kernel, and output arrays
+    float **image = (float **)malloc(IMAGE_SIZE * sizeof(float *));
+    float **output = (float **)malloc(IMAGE_SIZE * sizeof(float *));
+    for (int i = 0; i < IMAGE_SIZE; i++) {
+        image[i] = (float *)malloc(IMAGE_SIZE * sizeof(float));
+        output[i] = (float *)malloc(IMAGE_SIZE * sizeof(float));
+    }
+
+    float **kernel = (float **)malloc(KERNEL_SIZE * sizeof(float *));
+    for (int i = 0; i < KERNEL_SIZE; i++) {
+        kernel[i] = (float *)malloc(KERNEL_SIZE * sizeof(float));
+    }
 
     // Populate the image with random values
     for (int i = 0; i < IMAGE_SIZE; i++) {
@@ -44,10 +52,12 @@ int main() {
         }
     }
 
-    // Define a simple 3x3 kernel (e.g., edge detection kernel)
-    kernel[0][0] = -1; kernel[0][1] = -1; kernel[0][2] = -1;
-    kernel[1][0] = -1; kernel[1][1] =  8; kernel[1][2] = -1;
-    kernel[2][0] = -1; kernel[2][1] = -1; kernel[2][2] = -1;
+    // Define a 5x5 kernel (edge detection kernel)
+    kernel[0][0] = -1; kernel[0][1] = -1; kernel[0][2] = -1; kernel[0][3] = -1; kernel[0][4] = -1;
+    kernel[1][0] = -1; kernel[1][1] =  1; kernel[1][2] =  1; kernel[1][3] =  1; kernel[1][4] = -1;
+    kernel[2][0] = -1; kernel[2][1] =  1; kernel[2][2] =  8; kernel[2][3] =  1; kernel[2][4] = -1;
+    kernel[3][0] = -1; kernel[3][1] =  1; kernel[3][2] =  1; kernel[3][3] =  1; kernel[3][4] = -1;
+    kernel[4][0] = -1; kernel[4][1] = -1; kernel[4][2] = -1; kernel[4][3] = -1; kernel[4][4] = -1;
 
     // Start measuring execution time
     clock_t start = clock();
@@ -62,6 +72,18 @@ int main() {
     double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
     printf("Execution time (CPU convolution): %f seconds\n", time_spent);
 
+    // Free dynamically allocated memory
+    for (int i = 0; i < IMAGE_SIZE; i++) {
+        free(image[i]);
+        free(output[i]);
+    }
+    free(image);
+    free(output);
+
+    for (int i = 0; i < KERNEL_SIZE; i++) {
+        free(kernel[i]);
+    }
+    free(kernel);
+
     return 0;
 }
-
