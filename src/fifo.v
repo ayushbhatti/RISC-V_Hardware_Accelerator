d004d2f0fa76f4d54bb6bc57f7b8a5e51b03ab92
Owen <osowatzke@gmail.com>
2024-11-18 01:58:59 -0700

    Finishing FIFO implementation

--- src/fifo.v
+++ src/fifo.v
@@ -9,17 +9,13 @@ module fifo (
     rdDataOut,
     rdValidOut,
     rdReadyIn);
-
-    // Fifo parameters
+    
+    // FIFO parameters
+    parameter FIFO_STYLE = "RAM";
     parameter DATA_WIDTH = 32;
     parameter FIFO_DEPTH = 512;
     parameter FIFO_SKID  = 0;
-
-    // Derived FIFO parameters
-    localparam ADDR_WIDTH  = $clog2(FIFO_DEPTH);
-    localparam COUNT_WIDTH = $clog2(FIFO_DEPTH+1);
-    localparam ALMOST_FULL = FIFO_DEPTH - FIFO_SKID;
-
+    
     // Inputs and Outputs
     input  clkIn;
     input  rstIn;
@@ -32,151 +28,44 @@ module fifo (
     output rdValidOut;
     input  rdReadyIn;
     
-    // Control Registers
-    reg [COUNT_WIDTH-1:0] countR;
-
-    reg wrReadyR;
-    reg rdValidR;
-    reg fullR;
-    reg initR;
-
-    reg [ADDR_WIDTH-1:0] wrAddrR;
-    reg [ADDR_WIDTH-1:0] rdAddrR;
-
-    wire wrEn;
-    wire rdEn;
-
-    // Only write when there will be no overflows
-    assign wrEn = wrValidIn & (!fullR | rdEn);
-    
-    // Only read when the data is valid
-    assign rdEn = rdReadyIn & rdValidR;
-
-    always @(posedge clkIn) begin
-        if (rstIn) begin
-            countR              <= 0;
-            wrReadyR            <= 0;
-            rdValidR            <= 0;
-            fullR               <= 0;
-            initR               <= 1;
-            wrAddrR             <= 0;
-            rdAddrR             <= 2;
-        end else begin
-            
-            // Determine next count and
-            // control signals based on next count
-            if (wrEn && !rdEn) begin
-                countR          <= countR + 1;
-                if (countR == (ALMOST_FULL - 1)) begin
-                    wrReadyR    <= 0;
-                end
-                if (countR == (FIFO_DEPTH - 1)) begin
-                    fullR       <= 1;
-                end
-                if (countR == 0) begin
-                    rdValidR    <= 1;
-                end
-            end else if (!wrEn && rdEn) begin
-                countR          <= countR - 1;
-                if (countR == ALMOST_FULL) begin
-                    wrReadyR    <= 1;
-                end
-                if (countR == FIFO_DEPTH) begin
-                    fullR       <= 0;
-                end
-                if (countR == 1) begin
-                    rdValidR    <= 0;
-                end
-            end
-
-            // Enable write ready as soon as reset is deasserted
-            initR               <= 0;
-            if (initR) begin
-                wrReadyR        <= 1;
-            end
-
-            // Update write pointer
-            if (wrEn) begin
-                wrAddrR         <= wrAddrR + 1;
-            end
-
-            // Update read pointer
-            if (rdEn) begin
-                rdAddrR         <= rdAddrR + 1;
-            end
-
-            // Overflow detection
-            if (wrValidIn && fullR && !rdEn) begin
-                $error("Fifo overflow detected at time %t", $realtime);
-            end
-        end
-    end
-
-    // Ram signals
-    reg [DATA_WIDTH-1:0] ram [0:FIFO_DEPTH-1];
-    reg [DATA_WIDTH-1:0] rdData;
-
-    // Instantiate RAM
-    always @(posedge clkIn) begin
-        rdData           <= ram[rdAddrR];
-        if (wrEn) begin
-            ram[wrAddrR] <= wrDataIn;
+    // Validate FIFO style
+    initial begin
+        if ((FIFO_STYLE != "RAM") && (FIFO_STYLE != "SRL")) begin
+            $error("Unsupported FIFO style \"%s\". Must be either \"RAM\" or \"SRL\"", FIFO_STYLE);
         end
     end
     
-    // Read pipeline signals
-    reg [DATA_WIDTH-1:0] rdPipeDataR [1:0];
-    reg [1:0] rdPipeValidR;
-    reg rdEnR;
-    
-    
-    always @(posedge clkIn) begin
-        if (rstIn) begin
-            rdEnR           <= 0;
-            rdPipeValidR    <= 0;
-            rdPipeDataR[0]  <= 0;
-            rdPipeDataR[1]  <= 0;
-        end else begin
+    generate
+        if (FIFO_STYLE == "RAM") begin
         
-            // Data from memory is valid with 1 sample of delay
-            rdEnR                   <= rdEn;
+            fifo_sdpr #(
+                .DATA_WIDTH(DATA_WIDTH),
+                .FIFO_DEPTH(FIFO_DEPTH),
+                .FIFO_SKID(FIFO_SKID)) fifo_i(
+                .clkIn(clkIn),
+                .rstIn(rstIn),
+                .wrDataIn(wrDataIn),
+                .wrValidIn(wrValidIn),
+                .wrReadyOut(wrReadyOut),
+                .rdDataOut(rdDataOut),
+                .rdValidOut(rdValidOut),
+                .rdReadyIn(rdReadyIn));
+                
+        end else if (FIFO_STYLE == "SRL") begin
         
-            // Advance pipeline on read
-            if (rdEn) begin
-                rdPipeValidR        <= {1'b0, rdPipeValidR[1]};
-                rdPipeDataR[0]      <= rdPipeDataR[1];
-            end
-            
-            // Data read from memory is valid
-            if (rdEnR) begin
-                // Place data at start of pipeline if there are no valid samples
-                if (rdEn && !rdPipeValidR[1]) begin
-                    rdPipeValidR[0] <= 1;
-                    rdPipeDataR [0] <= rdData;
-                // Place data at end of pipeline if last sample is invalid
-                end else if (rdEn || !rdPipeValidR[1]) begin
-                    rdPipeValidR[1] <= 1;
-                    rdPipeDataR [1] <= rdData;
-                end
-            end
-        
-            // Place writes directly in read pipeline
-            // Handles first few writes
-            if (wrEn) begin
-                if ((countR == 0) || ((countR == 1) && rdEn)) begin
-                    rdPipeValidR[0] <= 1;
-                    rdPipeDataR [0] <= wrDataIn;
-                end else if (((countR == 1) & !rdEn) || ((countR == 2) && rdEn)) begin
-                    rdPipeValidR[1] <= 1;
-                    rdPipeDataR [1] <= wrDataIn;
-                end
-            end
+            fifo_srl #(
+                .DATA_WIDTH(DATA_WIDTH),
+                .FIFO_DEPTH(FIFO_DEPTH),
+                .FIFO_SKID(FIFO_SKID)) fifo_i(
+                .clkIn(clkIn),
+                .rstIn(rstIn),
+                .wrDataIn(wrDataIn),
+                .wrValidIn(wrValidIn),
+                .wrReadyOut(wrReadyOut),
+                .rdDataOut(rdDataOut),
+                .rdValidOut(rdValidOut),
+                .rdReadyIn(rdReadyIn));
+                
         end
-    end
-    
-    // Assign outputs
-    assign wrReadyOut   = wrReadyR;
-    assign rdDataOut    = rdPipeDataR[0];
-    assign rdValidOut   = rdValidR;
-
+    endgenerate
 endmodule
--- tb/fifo_tb.v
+++ tb/fifo_tb.v
@@ -1,5 +1,3 @@
-`timescale 1ns/1ns
-
 module fifo_tb;
 
     // Parameters
@@ -8,150 +6,122 @@ module fifo_tb;
     
     parameter FIFO_DEPTH = 32;
     parameter FIFO_SKID  = 4;
-
-    // Derived parameters based on inputs
-    localparam COUNT_WIDTH = $clog2(FIFO_DEPTH+1);
-
+    
     wire clk;
     wire rst;
 
     clk_gen #(.CLK_PERIOD(CLK_PERIOD)) clk_gen_i (.clkOut(clk));
     rst_gen #(.RESET_TIME(RESET_TIME)) rst_gen_i (.rstOut(rst));
-
-    localparam INIT     = 0;
-    localparam WRITE    = 1;
-    localparam WAIT     = 2;
-    localparam READ     = 3;
-
-    reg [1:0] stateR;
-    reg initR;
-
-    reg wrValidR;
-    reg [31:0] wrDataR;
-
-    reg rdReadyR;
-    reg [31:0] rdDataR;
-
-    reg [COUNT_WIDTH-1:0] countR;
-    reg [COUNT_WIDTH-1:0] maxCountR;
-
-    wire wrReady;
-    wire rdValid;
-    wire [31:0] rdData;
     
-    always @(posedge clk) begin
-        if (rst) begin
-            stateR      <= INIT;
-            wrValidR    <= 0;
-            rdReadyR    <= 0;
-            wrDataR     <= 0;
-            rdDataR     <= 0;
-            countR      <= 0;
-            maxCountR   <= FIFO_DEPTH;
-            initR       <= 0;
-        end else begin
-            wrValidR    <= 0;
-            rdReadyR    <= 0;
-            initR       <= 1;
-            if (wrValidR) begin
-                wrDataR <= wrDataR + 1;
-            end
-            if (rdReadyR) begin
-                rdDataR <= rdDataR + 1;
-            end
-            case (stateR)
-                INIT : begin
-                    if (initR) begin                   
-                        stateR      <= WRITE;
-                        countR      <= 0;
-                        if (maxCountR == FIFO_DEPTH) begin
-                            maxCountR   <= 1;
-                        end else begin
-                            maxCountR   <= maxCountR + 1;
-                        end
-                        if (!wrReady) begin
-                            $error("Expected wrReady = '1', Received wrReady = '0'");
-                        end
-                        if (rdValid) begin
-                            $error("Expected rdValid = '0', Received rdValid = '1'");
-                        end
-                    end
-                end
-                WRITE : begin
-                    wrValidR    <= 1;
-                    countR      <= countR + 1;
-                    if (countR < 2) begin
-                        if (rdValid) begin
-                            $error("Expected rdValid = '0', Received rdValid = '1'");
-                        end
-                    end else begin
-                        if (!rdValid) begin
-                            $error("Expected rdValid = '1', Received rdValid = '0'");
-                        end
-                    end
-                    if (countR <= (FIFO_DEPTH - FIFO_SKID)) begin
-                        if (!wrReady) begin
-                            $error("Expected wrReady = '1', Received wrReady = '0'");
-                        end
-                    end else begin
+    reg [31:0] wrDataR;
+    reg wrValidR;
+    
+    reg [31:0] rdDataR;
+    reg rdReadyR;
+    
+    wire [31:0] wrDataPipe;
+    wire wrValidPipe;
+    
+    wire [31:0] rdData;
+    wire rdValid;
+    wire wrReady;
+    
+    generate
+    
+        if (1) begin
+            reg [5:0] countR;
+            reg [3:0] probR;
+            
+            always @(posedge clk) begin
+                if (rst) begin
+                    wrValidR        <= 0;
+                    wrDataR         <= 0;
+                    probR           <= 4'h8;
+                    countR          <= 0;
+                end else begin
+                    wrValidR        <= 0;
+                    if ({1'b0, $random} < {probR, {29{1'b0}}}) begin
                         if (wrReady) begin
-                            $error("Expected wrReady = '0', Received wrReady = '1'");
-                        end
-                    end
-                    if (countR == (maxCountR - 1)) begin
-                        if (maxCountR == 1) begin
-                            stateR  <= READ;
-                        end else begin
-                            stateR  <= READ;
-                        end
-                    end
-                end
-                /*WAIT : begin
-                    if (rdValid) begin
-                        $error("Expected rdValid = '0', Received rdValid = '1'");
-                    end
-                    stateR          <= READ;
-                end*/
-                READ : begin
-                    rdReadyR        <= 1;
-                    if (rdReadyR) begin
-                        if (!rdValid) begin
-                            $error("Expected rdValid = '1', Received rdValid = '0'");
-                        end
-                        if (rdData !== rdDataR) begin
-                            $error("Expected rdData = 0x%08X, Received rdData = 0x%08X", rdDataR, rdData);
-                        end
-                        countR      <= countR - 1;
-                        if (countR == 1) begin
-                            stateR      <= INIT;
-                            rdReadyR    <= 0;
-                        end
-                    end else begin
-                        if (countR == 1) begin
-                            if (rdValid) begin
-                                $error("Expected rdValid = '0', Received rdValid = '1'");
-                            end
-                        end else begin
-                            if (!rdValid) begin
-                                $error("Expected rdValid = '1', Received rdValid = '0'");
+                            wrValidR    <= 1;
+                            wrDataR     <= wrDataR + 1;
+                            countR      <= countR + 1;
+                            if (countR == 63) begin
+                                countR  <= 0;
+                                probR   <= probR >> 1;
+                                if (probR == 1) begin
+                                    probR   <= 4'h8;
+                                end
                             end
                         end
                     end
                 end
-            endcase
+            end
         end
-    end
-
+    endgenerate
+    
+    delay #(
+        .DATA_WIDTH(1),
+        .LATENCY(FIFO_SKID-1)) delay_valid(
+        .clkIn(clk),
+        .rstIn(rst),
+        .dataIn(wrValidR),
+        .dataOut(wrValidPipe));
+    
+    delay #(
+        .DATA_WIDTH(32),
+        .LATENCY(FIFO_SKID-1)) delay_data(
+        .clkIn(clk),
+        .rstIn(rst),
+        .dataIn(wrDataR),
+        .dataOut(wrDataPipe));
+    
     fifo #(
         .FIFO_DEPTH(FIFO_DEPTH),
         .FIFO_SKID(FIFO_SKID)) fifo_i(
         .clkIn(clk),
         .rstIn(rst),
-        .wrDataIn(wrDataR),
-        .wrValidIn(wrValidR),
+        .wrDataIn(wrDataPipe),
+        .wrValidIn(wrValidPipe),
         .wrReadyOut(wrReady),
         .rdDataOut(rdData),
         .rdValidOut(rdValid),
         .rdReadyIn(rdReadyR));
-
+        
+    generate
+        
+        if (1) begin
+            reg [8:0] countR;
+            reg [3:0] probR;
+            
+            always @(posedge clk) begin
+                if (rst) begin
+                    rdReadyR        <= 0;
+                    rdDataR         <= 1;
+                    probR           <= 4'h8;
+                    countR          <= 0;
+                end else begin
+                    rdReadyR        <= 0;
+                    if ({1'b0, $random} < {probR, {29{1'b0}}}) begin
+                        rdReadyR    <= 1;
+                    end
+                    if (rdValid) begin
+                        if (rdReadyR) begin
+                            if (rdDataR !== rdData) begin
+                                $error("Error Detected at Time %t. Expected 0x%08X, Received 0x%08X", $realtime, rdDataR, rdData);
+                            end
+                            rdDataR     <= rdDataR + 1;
+                            countR      <= countR + 1;
+                            if (countR == 255) begin
+                                countR  <= 0;
+                                probR   <= probR >> 1;
+                                if (probR == 1) begin
+                                    probR   <= 4'h8;
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    endgenerate
 endmodule
