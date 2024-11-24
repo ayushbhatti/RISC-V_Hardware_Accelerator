db3452ca95c3fda75c58412e0e92448ee6ccc4e5
Owen <osowatzke@gmail.com>
2024-11-24 00:30:30 -0700

    Getting something without any obvious compile errors

--- src/cnn_hw_accelerator2.v
+++ src/cnn_hw_accelerator2.v
@@ -9,47 +9,47 @@ module cnn_hw_accelerator (
     addrIn;
     wrEnIn;
     wrDataIn;
-    dataIn;
     readyIn;
     validOut;
-    dataOut;
+    dataOut);
 
     // Configuration of RISCV bus interface
-    parameter BUS_ADDR_WIDTH  = 32;
-    parameter BUS_DATA_WIDTH  = 64;
-    parameter BUS_WE_WIDTH    = BUS_DATA_WIDTH/8;
+    parameter BUS_ADDR_WIDTH    = 32;
+    parameter BUS_DATA_WIDTH    = 64;
+    parameter BUS_WE_WIDTH      = BUS_DATA_WIDTH/8;
     
     // Floating-point hardware accelerator configuration
     // Standard single precision
-    parameter FRAC_WIDTH      = 24;
-    parameter EXP_WIDTH       = 8;
+    parameter FRAC_WIDTH        = 24;
+    parameter EXP_WIDTH         = 8;
     
     // Multiply and accumulate input width
-    parameter VECTOR_SIZE     = 8;
+    parameter VECTOR_SIZE       = 8;
     
     // Maximum size of input matrices (in elements)
-    parameter MAX_SIZE        = 4096;
+    parameter MAX_SIZE          = 4096;
+
+    // Derived counter width
+    parameter CNT_WIDTH         = $clog2(MAX_SIZE);
 
     // Derived vector size parameters
-    parameter VECTOR_SIZE_LOG2 = $clog2(VECTOR_SIZE);
+    localparam VECTOR_SIZE_LOG2 = $clog2(VECTOR_SIZE);
     
     // Derived RAM parameters
-    localparam RAM_DEPTH      = MAX_SIZE/VECTOR_SIZE;
-    localparam RAM_ADDR_WIDTH = $clog2(RAM_DEPTH);
-    localparam RAM_DATA_WIDTH = FRAC_WIDTH + EXP_WIDTH;
-    localparam RAM_WE_WIDTH   = RAM_DATA_WIDTH/8;  
-    
-    // Constants for selecting relevant bits of address
-    localparam NUM_BYTES      = RAM_WE_WIDTH*VECTOR_SIZE;
-    localparam ADDR_LO        = $clog2(NUM_BYTES);
-    localparam ADDR_HI        = RAM_ADDR_WIDTH + ADRR_LO - 1;
-
-    // Constants for mapping write enable bits
-    localparam GROUP_SIZE     = BUS_WE_WIDTH/RAM_WE_WIDTH
-    localparam NUM_GROUPS     = NUM_BYTES/BUS_WE_WIDTH
-        
+    localparam RAM_DEPTH        = MAX_SIZE/VECTOR_SIZE;
+    localparam RAM_ADDR_WIDTH   = $clog2(RAM_DEPTH);
+    localparam RAM_DATA_WIDTH   = FRAC_WIDTH + EXP_WIDTH;
+    localparam RAM_WE_WIDTH     = RAM_DATA_WIDTH/8;
+      
+    // Input/Output Ports
     input clkIn;
     input rstIn;
+    
+    input startIn;
+    input [CNT_WIDTH:0] filtRowsIn;
+    input [CNT_WIDTH:0] filtColsIn;
+    input [CNT_WIDTH:0] dataRowsIn;
+    input [CNT_WIDTH:0] dataColsIn;
     
     input [BUS_ADDR_WIDTH-1:0] addrIn;
     input [  BUS_WE_WIDTH-1:0] wrEnIn;
@@ -57,69 +57,102 @@ module cnn_hw_accelerator (
     
     input  readyIn;
     output validOut;
-    output [DATA_WIDTH-1:0] dataOut;
+    output [RAM_DATA_WIDTH-1:0] dataOut;
     
-    reg [ADDR_WIDTH-1:0] addrR [0:VECTOR_SIZE-1];
-    reg validR [VECTOR_SIZE-1];
-    reg lastR;
+    // Bus write registers
+    reg [RAM_ADDR_WIDTH-1:0] busAddrR;
+    reg [RAM_DATA_WIDTH-1:0] busDataR [0:VECTOR_SIZE-1];
+    reg [  RAM_WE_WIDTH-1:0] busWrEnR [0:1][0:VECTOR_SIZE-1];
     
-    wire fifoWrReady;
-    
-    // 
+    // Map Bus Writes to Correct RAM Banks
     genvar i;
     generate
+    
+        // Constants for selecting relevant bits of address
+        localparam NUM_BYTES    = RAM_WE_WIDTH*VECTOR_SIZE;
+        localparam ADDR_LO      = $clog2(NUM_BYTES);
+        localparam ADDR_HI      = RAM_ADDR_WIDTH + ADRR_LO;
+
+        // Constant for selecting write select bits
+        localparam WR_SEL_LO    = RAM_WE_WIDTH;
+        localparam WR_SEL_HI    = WR_SEL_LO + VECTOR_SIZE - 1;
+        
+        // Constants for mapping write enable bits
+        localparam GROUP_SIZE   = BUS_WE_WIDTH/RAM_WE_WIDTH
+        localparam NUM_GROUPS   = NUM_BYTES/BUS_WE_WIDTH
+    
+        // Extract address and write select bits
+        wire [RAM_ADDR_WIDTH:0] busAddr;
+        wire [VECTOR_SIZE_LOG2-1:0] busWrSel;
+        
+        // Get address and write select bits (applies to all groups)
+        assign busAddr  = addrIn[  ADDR_HI:ADDR_LO  ];
+        assign busWrSel = addrIn[WR_SEL_HI:WR_SEL_LO];
         
         for (i = 0; i < VECTOR_SIZE; i = i + 1) begin
         
+            // Determine indices of write enable bits
             localparam WE_LO   = (i % GROUP_SIZE) * RAM_WE_WIDTH;
             localparam WE_HI   = WE_LO + RAM_WE_WIDTH - 1;
+            
+            // Determine indices of data bits
             localparam DATA_LO = (i % GROUP_SIZE) * RAM_DATA_WIDTH;
             localparam DATA_HI = DATA_LO + RAM_DATA_WIDTH - 1;
+
+            // Extracted data and write enable bits
+            wire [  RAM_DATA_WIDTH-1:0] busWrData;
+            wire [    RAM_WE_WIDTH-1:0] busWrEn;
             
-            wire [RAM_ADDR_WIDTH-1:0] busAddr;
-            wire [RAM_DATA_WIDTH-1:0] busWrData;
-            wire [  RAM_WE_WIDTH-1:0] busWrEn;
-            
-            assign busAddr   = addrIn  [ADDR_HI:ADDR_LO];
+            // Get write data and write enable bits for group
             assign busWrData = wrDataIn[DATA_HI:DATA_LO];
             assign busWrEn   = wrEnIn  [  WE_HI:WE_LO  ];
 
-              
-        
-        if (NUM_BYTES >= BUS_WE_WIDTH) begin
-            ADDR_WIDTH = NUM_BYTES/BUS_WE_WIDTH
-        end else begin
-            
-        end
-        
-        if (addrIn(WE
-        for (i = 0; i < VECTOR_SIZE; i = i + 1) begin
-            sp_ram mem_i(
-                .clkIn(clkIn),
-                .rstIn(rstIn),
-                .addrIn(addrR[i]),
-                .wrEnIn(),
-                .wrDataIn(),
-                .rdEnIn(validR[i]),
-                .rdDataOut(ramData[i]),
-                .rdAckOut(ramValid[i]))
-        end
-    endgenerate
-    
-    baseAddrR   <= baseAddrR + 1;
-    
-    generate
-        for (i = 0; i < VECTOR_SIZE; i = i + 1) begin
+            // Map writes to correct set of write enable bits
+            integer j;
             always @(posedge clkIn) begin
-                addrR[i] <= addrR[i] + VECTOR_SIZE;
-                if (addrR[i] < endAddrR) begin
-                    addrR[i] <= ;
-                else
-                    addrR[i] <= addrR[i] + VECTOR_SIZE;
+                busWrDataR[i] <= busWrData;
+                for (j = 0; j < 2; j = j + 1) begin
+                    busWrEnR[j][i] <= 0;
+                    if ((busAddr[RAM_ADDR_WIDTH] == j) && (busWrSel == i)) begin
+                        busWrEnR[j][i] <= busWrEn;
+                    end
                 end
             end
         end
+        
+        // Select only required bits of address
+        always @(posedge clkIn) begin
+            busWrAddrR <= busWrAddr[RAM_DATA_WIDTH-1:0];
+        end        
     endgenerate
+    
+    // State Definitions
+    localparam IDLE = 0;
+    localparam INIT = 1;
+    localparam CALC = 2;
+    
+    // Parameters for selecting subregions of column counts
+    localparam COL_CNT_WIDTH = CNT_WIDTH - VECTOR_SIZE_LOG2;
+    localparam COL_CNT_LO    = VECTOR_SIZE_LOG2;
+    localparam COL_CNT_HI    = CNT_WIDTH - 1;
+    
+    // Number of input cells
+    reg numCellsR;
+    
+    // FSM registers
+    reg [1:0] stateR;
+    reg validR;
+    
+    reg [VECTOR_SIZE_LOG2-1:0] lastRdCntR;
+    reg [COL_CNT_WIDTH-1:0] maxColCntR;
+    reg [CNT_WIDTH-1:0] maxRowCntR;
+    reg [CNT_WIDTH-1:0] maxBaseCntR;
+    
+    // Column Index Counter
+    wire colAdv;
+    wire colClr;
+    wire colDoneR;
+    wire [COL_CNT_WIDTH-1:0] colCntR;
     
     // Row Index Counter
     wire rowAdv;
@@ -127,75 +160,35 @@ module cnn_hw_accelerator (
     wire rowDoneR;
     wire [CNT_WIDTH-1:0] rowCntR;
     
-    // Column Index Counter
-    wire colAdv;
-    wire colClr;
-    wire colDoneR;
-    wire [CNT_WIDTH-1:0] colCntR;
-    
     // Base Address Counter
     wire baseAdv;
     wire baseClr;
     wire baseDoneR;
     wire [CNT_WIDTH-1:0] baseCntR;
     
-    // Counter control signals
-    assign baseAdv = rowDoneR & colDoneR & fifoWrReady;
-    assign baseClr = !validR & fifoWrReady;
+    // Done signal
+    wire done;
     
-    assign colAdv  = rowDoneR;
-    assign colClr  = baseClr | baseAdv;
+    // Get number of input cells
+    always @(posedge clkIn) begin
+        numCellsR <= dataRowsIn * dataColsIn;
+    end
     
-    assign rowAdv  = 1;
-    assign rowClr  = colClr | (colAdv & !colClr);
-    
-    // Row Index Counter
-    counter #(
-        .CNT_WIDTH(CNT_WIDTH)) row_cnt (
-        .clkIn(clkIn),
-        .rstIn(1'b0),
-        .clrIn(rowClr),
-        .advIn(rowAdv),
-        .endValIn(maxRowCntR),
-        .cntOut(rowCntR),
-        .doneOut(rowDoneR));
-       
-    // Column Index Counter
-    counter #(
-        .CNT_WIDTH(CNT_WIDTH)) col_cnt (
-        .clkIn(clkIn),
-        .rstIn(1'b0),
-        .clrIn(colClr),
-        .advIn(colAdv),
-        .endValIn(maxColCntR),
-        .cntOut(colCntR),
-        .doneOut(colDoneR));
-      
-    // Base Address Counter
-    counter #(
-        .CNT_WIDTH(CNT_WIDTH)) base_cnt (
-        .clkIn(clkIn),
-        .rstIn(1'b0),
-        .clrIn(baseClr),
-        .advIn(baseAdv),
-        .endValIn(maxAddrR),
-        .cntOut(baseCntR),
-        .doneOut(baseDoneR));
-    
-    // Done signal for 2D Convolution 
-    assign done = rowDoneR & colDoneR & baseDoneR;
-    
+    // Read state machine
     always @(posedge clkIn) begin
         if (rstIn) begin
-            stateR  <= IDLE;
-            validR  <= 0;
+            stateR      <= IDLE;
+            validR      <= 0;
+            lastRdCntR  <= 0;
+            maxColCntR  <= 0;
+            maxRowCntR  <= 0;
+            maxBaseCntR <= 0;
         end else begin
             case (stateR)
                 IDLE : begin
-                    numCellsR   <= dataRowsIn * dataColsIn;
-                    maxColCntR  <= filtColsIn - 1;
-                    maxRowCntR  <= filtRowsIn[ROWS_HI:ROWS_LO] - 1;
-                    lastRowCntR <= filtRowsIn[ROWS_LO-1:0] - 1;
+                    maxColCntR  <= filtColsIn[COL_CNT_HI:COL_CNT_LO] - 1;
+                    lastRdCntR  <= filtColsIn[COL_CNT_LO-1:0] - 1;
+                    maxRowCntR  <= filtRowsIn - 1;
                     if (startIn) begin
                         stateR  <= INIT;
                     end
@@ -215,19 +208,92 @@ module cnn_hw_accelerator (
         end
     end
     
+    // Counter control signals
+    assign baseAdv = colDoneR & rowDoneR & fifoWrReady;
+    assign baseClr = !validR & fifoWrReady;
+    
+    assign rowAdv  = rowDoneR;
+    assign rowClr  = baseClr | baseAdv;
+    
+    assign colAdv  = 1;
+    assign colClr  = rowClr | (rowAdv & !rowClr);
+       
+    // Column Index Counter
+    counter #(
+        .CNT_WIDTH(COL_CNT_WIDTH)) col_cnt (
+        .clkIn(clkIn),
+        .rstIn(1'b0),
+        .clrIn(colClr),
+        .advIn(colAdv),
+        .endValIn(maxColCntR),
+        .cntOut(colCntR),
+        .doneOut(colDoneR));
+      
+    // Row Index Counter
+    counter #(
+        .CNT_WIDTH(CNT_WIDTH)) row_cnt (
+        .clkIn(clkIn),
+        .rstIn(1'b0),
+        .clrIn(rowClr),
+        .advIn(rowAdv),
+        .endValIn(maxRowCntR),
+        .cntOut(rowCntR),
+        .doneOut(rowDoneR));
+        
+    // Base Address Counter
+    counter #(
+        .CNT_WIDTH(CNT_WIDTH)) base_cnt (
+        .clkIn(clkIn),
+        .rstIn(1'b0),
+        .clrIn(baseClr),
+        .advIn(baseAdv),
+        .endValIn(maxAddrR),
+        .cntOut(baseCntR),
+        .doneOut(baseDoneR));
+    
+    // Done signal for 2D Convolution 
+    assign done = colDoneR & rowDoneR & baseDoneR;
+    
+    // Pipeline #2
+    reg last2R;
+    reg [CNT_WIDTH-1:0] addr2R;
+    reg [CNT_WIDTH-1:0] offset2R;
+    reg [CNT_WIDTH-1:0] colCnt2R;
+    
+    // Pipeline #3
+    reg last3R;
+    reg [CNT_WIDTH-1:0] dataAddr3R;
+    reg [CNT_WIDTH-1:0] filtAddr3R;
+    
+    // Pipeline #4
+    reg last4R;
+    reg [VECTOR_SIZE_LOG2-1:0] dataShift4R;
+    reg [VECTOR_SIZE_LOG2-1:0] filtShift4R;
+    reg [CNT_WIDTH-1:0] dataAddrVar;
+    reg [CNT_WIDTH-1:0] filtAddrVar;
+    reg [CNT_WIDTH-1:0] dataAddr4R;
+    reg [CNT_WIDTH-1:0] filtAddr4R;
+    
+    // Pipeline #5
+    reg last5R;
+    reg [VECTOR_SIZE_LOG2-1:0] dataShift5R;
+    reg [VECTOR_SIZE_LOG2-1:0] filtShift5R;
+    reg [CNT_WIDTH-1:0] dataAddr5R;
+    reg [CNT_WIDTH-1:0] filtAddr5R;
+    
     // Data Process
     always @(posedge clkIn) begin
     
         // Pipeline #2
-        last2R   <= rowDoneR & colDoneR;
-        addr2R   <= {rowCntR, VECTOR_SIZE_LOG2{1'b0}} * colCntR;
-        offset2R <= {rowCntR, VECTOR_SIZE_LOG2{1'b0}} + baseCntR:
-        rowCnt2R <= {rowCntR, VECTOR_SIZE_LOG2{1'b0}};
+        last2R   <= colDoneR & rowDoneR;
+        addr2R   <= {colCntR, COL_CNT_LO{1'b0}} * rowCntR;
+        offset2R <= {colCntR, COL_CNT_LO{1'b0}} + baseCntR:
+        colCnt2R <= {colCntR, COL_CNT_LO{1'b0}};
         
         // Pipeline #3
         last3R      <= last2R;
         dataAddr3R  <= addr2R + offset2R;
-        filtAddr3R  <= addr2R + rowCnt2R;
+        filtAddr3R  <= addr2R + colCnt2R;
         
         // Pipeline #4
         last4R      <= last3R;
@@ -255,9 +321,16 @@ module cnn_hw_accelerator (
         filtAddr5R  <= (filtAddr4R << (filtShift4R*RAM_ADDR_WIDTH)) | (filtAddr4R >> ((VECTOR_SIZE - filtShift4R)*RAM_ADDR_WIDTH));
     end
     
+    // Pipeline #2
     reg [VECTOR_SIZE-1:0] rdEn2R;
+    
+    // Pipeline #3
     reg [VECTOR_SIZE-1:0] rdEn3R;
+    
+    // Pipeline #4
     reg [VECTOR_SIZE-1:0] rdEn4R;
+    
+    // Pipeline #5
     reg [VECTOR_SIZE-1:0] dataRdEn5R;
     reg [VECTOR_SIZE-1:0] filtRdEn5R;
     
@@ -275,7 +348,7 @@ module cnn_hw_accelerator (
             // Determine which bits of read enable are high
             for (i = 0; i < VECTOR_SIZE; i = i + 1) begin
                 if (rowDoneR) begin
-                    if (i < lastRowCntR) begin
+                    if (i < lastRdCntR) begin
                         rdEn2R[i] <= validR;
                     end else begin
                         rdEn2R[i] <= 0;
@@ -298,10 +371,23 @@ module cnn_hw_accelerator (
         end
     end
     
+    // RAM constants
     localparam WREN_ZERO  = {VECTOR_SIZE{1'b0}};
     localparam DATA_ZERO  = { DATA_WIDTH{1'b0}};
     localparam RD_LATENCY = 1;
-                
+       
+    // RAM Bank A
+    wire [DATA_WIDTH-1:0] dataA [0:VECTOR_SIZE-1];
+    wire [VECTOR_SIZE_LOG2-1:0] dataAShift;
+    
+    // RAM Bank B
+    wire [DATA_WIDTH-1:0] dataB [0:VECTOR_SIZE-1];
+    wire [VECTOR_SIZE_LOG2-1:0] dataBShift;
+    
+    // Common RAM signals
+    wire [VECTOR_SIZE-1:0] valid;
+    wire ramLast;
+    
     // Generate Data RAM for each vector element
     generate
         for (i = 0; i < VECTOR_SIZE; i = i + 1) begin
@@ -316,9 +402,9 @@ module cnn_hw_accelerator (
                 .RAM_DEPTH(RAM_DEPTH)) data_ram (
                 .clkIn(clkIn),
                 .rstIn(rstIn),
-                .addrAIn(busAddr[i]),
-                .wrEnAIn(busWrEn[i]),
-                .wrDataAIn(busWrData[i]),
+                .addrAIn(busAddrR),
+                .wrEnAIn(busWrEnR[0][i]),
+                .wrDataAIn(busWrDataR[i]),
                 .rdEnAIn(1'b0),
                 .addrBIn(rdAddr),
                 .wrEnBIn(WREN_ZERO),
@@ -329,14 +415,15 @@ module cnn_hw_accelerator (
                 
             assign dataA[DATA_WIDTH*i+:DATA_WIDTH] = rdData;
             
-            delay #(
-                .LATENCY(RD_LATENCY),
-                .DATA_WIDTH(VECTOR_SIZE_LOG2)) data_delay (
-                .clkIn(clkIn),
-                .rstIn(1'b0),
-                .dataIn(dataRdShift5R),
-                .dataOut(dataAShift));            
         end
+            
+        delay #(
+            .LATENCY(RD_LATENCY),
+            .DATA_WIDTH(VECTOR_SIZE_LOG2)) data_delay (
+            .clkIn(clkIn),
+            .rstIn(1'b0),
+            .dataIn(dataRdShift5R),
+            .dataOut(dataAShift)); 
     endgenerate
         
     // Generate Filter RAM for each vector element
@@ -353,9 +440,9 @@ module cnn_hw_accelerator (
                 .RAM_DEPTH(RAM_DEPTH)) filt_ram (
                 .clkIn(clkIn),
                 .rstIn(rstIn),
-                .addrAIn(busAddr[i]),
-                .wrEnAIn(busWrEn[i]),
-                .wrDataAIn(busWrData[i]),
+                .addrAIn(busAddrR),
+                .wrEnAIn(busWrEnR[1][i]),
+                .wrDataAIn(busWrDataR[i]),
                 .rdEnAIn(1'b0),
                 .addrBIn(rdAddr),
                 .wrEnBIn(WREN_ZERO),
@@ -365,25 +452,27 @@ module cnn_hw_accelerator (
                 
             assign dataB[DATA_WIDTH*i+:DATA_WIDTH] = rdData;
             
-            delay #(
-                .LATENCY(RD_LATENCY),
-                .DATA_WIDTH(VECTOR_SIZE_LOG2)) filt_delay (
-                .clkIn(clkIn),
-                .rstIn(1'b0),
-                .dataIn(filtRdShift5R),
-                .dataOut(dataBShift)); 
         end
+            
+        delay #(
+            .LATENCY(RD_LATENCY),
+            .DATA_WIDTH(VECTOR_SIZE_LOG2)) filt_delay (
+            .clkIn(clkIn),
+            .rstIn(1'b0),
+            .dataIn(filtRdShift5R),
+            .dataOut(dataBShift)); 
     endgenerate
     
     // Delay last signal to match reads from RAM
     delay #(
-        .LATENCY(RD_LATENCY+1),
+        .LATENCY(RD_LATENCY),
         .DATA_WIDTH(1)) last_delay (
         .clkIn(clkIn),
         .rstIn(1'b0),
         .dataIn(last5R),
         .dataOut(ramLast));
-                
+    
+    // RAM Output Pipeline Stage
     reg [VECTOR_SIZE-1:0] validR;
     reg [DATA_WIDTH*VECTOR_SIZE-1:0] dataAR;
     reg [DATA_WIDTH*VECTOR_SIZE-1:0] dataBR;
@@ -406,6 +495,7 @@ module cnn_hw_accelerator (
         dataBR      <= (dataB >> (dataBShift*DATA_WIDTH)) | (dataB << ((VECTOR_SIZE - dataBShift)*DATA_WIDTH));
     end
     
+    // Multiply and accumulate results
     wire [DATA_WIDTH-1:0] macData;
     wire macValid;
     
